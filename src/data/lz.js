27fc40ac31074eb79408807ee8f8a9f2913367fb
qiuping_wu <qiuping_wu@ot.olympus.co.jp>
2020-10-01 09:45:17 +0900

    update data

--- src/data/lz.js
+++ src/data/lz.js
@@ -1,26 +1,27 @@
 export default {
   lineList: [
-    { name: '1号线', color: '#3EB1FF', stations: [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 ] }
+    { name: '1号线', color: '#3EB1FF', stations: [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 ] }
   ],
   stationList: [
-    { id: 0, name: '陈官营', pinyin: 'chenguanying', py: 'cgy', lat: '36.092658', lng: '103.648173' },
-    { id: 1, name: '深安大桥南', pinyin: 'shenandaqiaonan', py: 'sadqn', lat: '36.091557', lng: '103.675199' },
-    { id: 2, name: '兰州城市学院（省科技馆）', pinyin: 'lanzhouchengshixueyuan(shengkejiguan)', py: 'lzcsxy(skjg)', lat: '36.099013', lng: '103.701538' },
-    { id: 3, name: '兰州海关', pinyin: 'lanzhouhaiguan', py: 'lzhg', lat: '36.093698', lng: '103.7104' },
-    { id: 4, name: '马滩', pinyin: 'matan', py: 'mt', lat: '36.083537', lng: '103.726901' },
-    { id: 5, name: '土门墩', pinyin: 'tumendun', py: 'tmd', lat: '36.073934', lng: '103.737517' },
-    { id: 6, name: '兰州西站北广场', pinyin: 'lanzhouxizhanbeiguangchang', py: 'lzxzbgc', lat: '36.071258', lng: '103.753294' },
-    { id: 7, name: '西站什字', pinyin: 'xizhanshizi', py: 'xzsz', lat: '36.068496', lng: '103.769768' },
-    { id: 8, name: '七里河', pinyin: 'qilihe', py: 'qlh', lat: '36.066372', lng: '103.78194' },
-    { id: 9, name: '小西湖', pinyin: 'xiaoxihu', py: 'xxh', lat: '36.064204', lng: '103.793645' },
-    { id: 10, name: '文化宫', pinyin: 'wenhuagong', py: 'whg', lat: '36.060509', lng: '103.807362' },
-    { id: 11, name: '西关', pinyin: 'xiguan', py: 'xg', lat: '36.059507', lng: '103.81735' },
-    { id: 12, name: '东方红广场', pinyin: 'dongfanghongguangchang', py: 'dfhgc', lat: '36.053713', lng: '103.83935' },
-    { id: 13, name: '兰州大学', pinyin: 'lanzhoudaxue', py: 'lzdx', lat: '36.04996', lng: '103.856553' },
-    { id: 14, name: '五里铺', pinyin: 'wulipu', py: 'wlp', lat: '36.046542', lng: '103.871273' },
-    { id: 15, name: '省气象局', pinyin: 'shengqixiangju', py: 'sqxj', lat: '36.04169', lng: '103.883521' },
-    { id: 16, name: '拱星墩', pinyin: 'gongxingdun', py: 'gxd', lat: '36.039686', lng: '103.892104' },
-    { id: 17, name: '焦家湾', pinyin: 'jiaojiawan', py: 'jjw', lat: '36.038402', lng: '103.908068' },
-    { id: 18, name: '东岗', pinyin: 'donggang', py: 'dg', lat: '36.040467', lng: '103.918196' }
+    { id: 0, name: '陈官营', pinyin: 'chenguanying', py: 'cgy', lat: '36.08694', lng: '103.662287' },
+    { id: 1, name: '深安大桥南', pinyin: 'shenandaqiaonan', py: 'sadqn', lat: '36.090547', lng: '103.678042' },
+    { id: 2, name: '兰州城市学院（省科技馆）', pinyin: 'lanzhouchengshixueyuanshengkejiguan', py: 'lzcsxy(skjg)', lat: '36.098896', lng: '103.701136' },
+    { id: 3, name: '兰州海关', pinyin: 'lanzhouhaiguan', py: 'lzhg', lat: '36.094509', lng: '103.709477' },
+    { id: 4, name: '马滩', pinyin: 'matan', py: 'mt', lat: '36.082909', lng: '103.727287' },
+    { id: 5, name: '土门墩', pinyin: 'tumendun', py: 'tmd', lat: '36.073062', lng: '103.743155' },
+    { id: 6, name: '兰州西站北广场', pinyin: 'lanzhouxizhanbeiguangchang', py: 'lzxzbgc', lat: '36.071462', lng: '103.752661' },
+    { id: 7, name: '西站什字', pinyin: 'xizhanshizi', py: 'xzsz', lat: '36.068497', lng: '103.769505' },
+    { id: 8, name: '七里河', pinyin: 'qilihe', py: 'qlh', lat: '36.066415', lng: '103.781259' },
+    { id: 9, name: '小西湖', pinyin: 'xiaoxihu', py: 'xxh', lat: '36.063532', lng: '103.796279' },
+    { id: 10, name: '文化宫', pinyin: 'wenhuagong', py: 'whg', lat: '36.061129', lng: '103.805189' },
+    { id: 11, name: '西关', pinyin: 'xiguan', py: 'xg', lat: '36.059381', lng: '103.816927' },
+    { id: 12, name: '省政府', pinyin: 'shengzhengfu', py: 'szf', lat: '36.059147', lng: '103.826599' },
+    { id: 13, name: '东方红广场', pinyin: 'dongfanghongguangchang', py: 'dfhgc', lat: '36.053193', lng: '103.842413' },
+    { id: 14, name: '兰州大学', pinyin: 'lanzhoudaxue', py: 'lzdx', lat: '36.049953', lng: '103.856597' },
+    { id: 15, name: '五里铺', pinyin: 'wulipu', py: 'wlp', lat: '36.046665', lng: '103.870678' },
+    { id: 16, name: '省气象局', pinyin: 'shengqixiangju', py: 'sqxj', lat: '36.041634', lng: '103.883472' },
+    { id: 17, name: '拱星墩', pinyin: 'gongxingdun', py: 'gxd', lat: '36.039114', lng: '103.895397' },
+    { id: 18, name: '焦家湾', pinyin: 'jiaojiawan', py: 'jjw', lat: '36.038303', lng: '103.907741' },
+    { id: 19, name: '东岗', pinyin: 'donggang', py: 'dg', lat: '36.040471', lng: '103.917767' }
   ]
 };
