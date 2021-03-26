e940b6f84b40db4920b00587756b7675b5ea4779
qiuping_wu <qiuping_wu@ot.olympus.co.jp>
2021-03-26 14:31:40 +0900

    cq jn ly

---
+++ src/data/ly.js
@@ -0,0 +1,26 @@
+export default {
+  lineList: [
+    { name: '1号线', color: '#F464B0', stations: [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 ] }
+  ],
+  stationList: [
+    { id: 0, name: '红山', pinyin: 'hongshan', py: 'hs', lat: '34.702306', lng: '112.330892' },
+    { id: 1, name: '谷水', pinyin: 'gushui', py: 'gs', lat: '34.682828', lng: '112.335344' },
+    { id: 2, name: '秦岭路', pinyin: 'qinlinglu', py: 'qll', lat: '34.677411', lng: '112.354721' },
+    { id: 3, name: '武汉路', pinyin: 'wuhanlu', py: 'whl', lat: '34.669558', lng: '112.367574' },
+    { id: 4, name: '长安路', pinyin: 'changanlu', py: 'cal', lat: '34.662657', lng: '112.376264' },
+    { id: 5, name: '上海市场', pinyin: 'shanghaishichang', py: 'shsc', lat: '34.659992', lng: '112.385362' },
+    { id: 6, name: '牡丹广场', pinyin: 'mudanguangchang', py: 'mdgc', lat: '34.655597', lng: '112.400683' },
+    { id: 7, name: '七里河', pinyin: 'qilihe', py: 'qlh', lat: '34.659939', lng: '112.411562' },
+    { id: 8, name: '王城公园', pinyin: 'wangchenggongyuan', py: 'wcgy', lat: '34.666452', lng: '112.426775' },
+    { id: 9, name: '解放路', pinyin: 'jiefanglu', py: 'jfl', lat: '34.670087', lng: '112.436453' },
+    { id: 10, name: '周王城广场', pinyin: 'zhouwangchengguangchang', py: 'zwcgc', lat: '34.672853', lng: '112.443799' },
+    { id: 11, name: '应天门', pinyin: 'yingtianmen', py: 'ytm', lat: '34.677654', lng: '112.456635' },
+    { id: 12, name: '丽景门', pinyin: 'lijingmen', py: 'ljm', lat: '34.682485', lng: '112.469717' },
+    { id: 13, name: '青年宫', pinyin: 'qingniangong', py: 'qng', lat: '34.684203', lng: '112.481276' },
+    { id: 14, name: '夹马营', pinyin: 'jiamaying', py: 'jmy', lat: '34.685983', lng: '112.495581' },
+    { id: 15, name: '启明南路', pinyin: 'qimingnanlu', py: 'qmnl', lat: '34.687924', lng: '112.504464' },
+    { id: 16, name: '塔湾', pinyin: 'tawan', py: 'tw', lat: '34.690076', lng: '112.517747' },
+    { id: 17, name: '史家湾', pinyin: 'shijiawan', py: 'sjw', lat: '34.692776', lng: '112.534784' },
+    { id: 18, name: '杨湾', pinyin: 'yangwan', py: 'yw', lat: '34.69641', lng: '112.554504' }
+  ]
+};
